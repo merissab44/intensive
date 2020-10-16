261278a70233e82cbda14ebe0108cee40e9a631c
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-10-15 20:06:16 -0700

    added carousel to client card

--- script.js
+++ script.js
@@ -4,3 +4,18 @@
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
